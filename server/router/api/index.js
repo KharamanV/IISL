3e9e850288bd5e239315d08186cd48d0d90b02d2
Vladimir <kharaman.v@gmail.com>
2017-10-10 21:25:17 +0300

    Add controllers folder to server

--- server/router/api/index.js
+++ server/router/api/index.js
@@ -3,8 +3,7 @@ const { auth, security } = require('./auth');
 
 router.use('/auth', auth);
 router.use(security());
-router.use('/games', require('./games'));
-router.use('/users', require('./users'));
-router.use('/profile', require('./profile'));
+
+// Private routes below
 
 module.exports = router;
